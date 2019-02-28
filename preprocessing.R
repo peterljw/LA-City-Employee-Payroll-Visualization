b30bbd454b5eda67d8b5377f1ea81c26183ef18e
peterljw <44175394+peterljw@users.noreply.github.com>
2019-02-28 14:11:46 -0800

    Update preprocessing.R

--- preprocessing.R
+++ preprocessing.R
@@ -1,47 +1,42 @@
 library(readr)
 library(tidyverse)
 library(reshape2)
-library(plotly)
-library(ggpubr)
 
-df <- read_csv("City_Employee_Payroll.csv")
+df <- read_csv("/home/m280data/la_payroll/City_Employee_Payroll.csv")
 
 
-# ----- Overview -----
 # Wide data of all types of payments for every year
 years <- unique(df$Year)
 payments <- list()
 for(year in years) {
   payments[[toString(year)]] <- df %>%
     filter(Year == year) %>%
-    select(`Base Pay`, `Overtime Pay`, `Permanent Bonus Pay`, `Longevity Bonus Pay`, 
+    select(`Total Payments`, `Base Pay`, `Overtime Pay`, `Permanent Bonus Pay`, `Longevity Bonus Pay`, 
            `Temporary Bonus Pay`, `Lump Sum Pay`, `Other Pay & Adjustments`) %>%
     sapply(function(x) sum(x, na.rm = T))
 }
 df.year <- data.frame(matrix(unlist(payments), nrow=length(years), byrow=T),stringsAsFactors=FALSE)
-colnames(df.year) <- c("Base Pay", "Overtime Pay", "Permanent Bonus Pay", "Longevity Bonus Pay", 
+colnames(df.year) <- c("Total","Base Pay", "Overtime Pay", "Permanent Bonus Pay", "Longevity Bonus Pay", 
                        "Temporary Bonus Pay", "Lump Sum Pay", "Other Pay & Adjustments")
 df.year <- cbind("Year" = years, df.year)
-saveRDS(df.year, "PaymentsByYear.RDS")
+# saveRDS(df.year, "/home/peterljw/biostat-m280-2019-winter/hw3/LA-City-Employee-Payroll/PaymentsByYear.RDS")
 
 # Long data of basic, overtime, and other for every year
 long.df <- melt(df.year, id.vars='Year')
 colnames(long.df) <- c("Year", "Payment", "Amount")
-saveRDS(long.df, "PaymentsByYear-Long.RDS")
+# saveRDS(long.df, "/home/peterljw/biostat-m280-2019-winter/hw3/LA-City-Employee-Payroll/PaymentsByYear-Long.RDS")
 
-
-# ----- Employee ----- 
+ 
 # Wide data of basic, overtime, and other for every employee 
 # (combined title and department into ID; sorted in descending order) 
 df <- df[order(-df$`Total Payments`),]
 df.individual <- within(df,  ID <- paste(`Job Class Title`, `Department Title`, sep="-"))
 df.individual <- df.individual %>%
-  select(`Year`, `Base Pay`, `Overtime Pay`, `Other Pay (Payroll Explorer)`, `ID`)
-colnames(df.individual) <- c("Year", "Base Pay", "Overtime Pay", "Other Pay", "ID")
-saveRDS(df.individual, "PaymentsByIndividual.RDS")
+  select(`Year`, `Total Payments`, `Base Pay`, `Overtime Pay`, `Other Pay (Payroll Explorer)`, `ID`)
+colnames(df.individual) <- c("Year", "Total", "Base Pay", "Overtime Pay", "Other Pay", "ID")
+# saveRDS(df.individual, "/home/peterljw/biostat-m280-2019-winter/hw3/LA-City-Employee-Payroll/PaymentsByIndividual.RDS")
 
 
-# ----- Department -----
 # Wide data of basic, overtime, and other for every department (sorted in descending order)
 depts <- unique(df$`Department Title`)
 years <- unique(df$Year)
@@ -61,10 +56,9 @@ df.dept[,3:6] <- sapply(df.dept[,3:6], function(x) as.numeric(as.character(unlis
 colnames(df.dept) <- c("Department", "Year", "Total", "Base Pay", "Overtime Pay", "Other Pay")
 df.dept <- df.dept[order(-df.dept$Total),]
 
-saveRDS(df.dept, "PaymentsByDept.RDS")
+# saveRDS(df.dept, "/home/peterljw/biostat-m280-2019-winter/hw3/LA-City-Employee-Payroll/PaymentsByDept.RDS")
 
 
-# ----- Benefit -----
 # Wide data of health, dental, and basic life cost for every department (sorted in descending order)
 depts <- unique(df$`Department Title`)
 years <- unique(df$Year)
@@ -84,4 +78,4 @@ df.benefit[,3:6] <- sapply(df.benefit[,3:6], function(x) as.numeric(as.character
 colnames(df.benefit) <- c("Department", "Year", "Total", "Health Cost", "Dental Cost", "Basic Life")
 df.benefit <- df.benefit[order(-df.benefit$Total),]
 
-saveRDS(df.benefit, "BenefitsByDept.RDS")
+# saveRDS(df.benefit, "/home/peterljw/biostat-m280-2019-winter/hw3/LA-City-Employee-Payroll/BenefitsByDept.RDS")
